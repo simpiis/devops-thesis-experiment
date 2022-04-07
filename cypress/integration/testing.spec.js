024911137d7c3862ebe176cf840d1fafafca405d
Carr99 <andre@carrblad.se>
2022-04-07 13:37:15 +0200

    Added more test to cypress

--- cypress/integration/testing.spec.js
+++ cypress/integration/testing.spec.js
@@ -2,9 +2,20 @@ describe("Integration Test", () => {
   // tests go here
   it("Can open first page", () => {
     cy.visit("/");
-    cy.contains("Hello World!")
+    cy.contains("Hello World!");
   });
   it("Is on login page", () => {
-    cy.location("href").should("eq", "http://localhost:3001/login") // possibly just /login
+    cy.visit("/login");
+    cy.url().should('include', '/login');
+  });
+  it("Can fill the login form", () => {
+    cy.get('input[id="username"]').type("André").should("have.value", "André");
+    cy.get('input[id="password"]').type("123456789");
+  });
+  it("Can submit the login form", () => {
+    cy.get("form").submit();
+  });
+  it("Goes to user page", () => {
+    cy.url().should('include', '/user');
   });
 });
